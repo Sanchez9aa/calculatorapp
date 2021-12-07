4fc84fd81669b0591e9320c2ad5d4b45979a9cd7
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-07 14:52:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Calculator from "./components/calculator/Calculator";
 import { createGlobalStyle } from "styled-components";
 
-
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@700&display=swap');
   *{
